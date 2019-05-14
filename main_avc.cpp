ce8cdcb32d9e48c2c40479f72577a9e42690f9c5
Taran John <taranjohn@gmail.com>
2019-05-14 16:08:39 +1200

    Update main_avc.cpp

---
+++ main_avc.cpp
@@ -0,0 +1,25 @@
+//Header Files
+#include <iostream>
+#include <stdio.h>
+#include "E101.h"
+#include <unistd.h>
+#include <time.h>
+
+/** Initialise Hardware */
+int main(){
+    
+
+}
+
+/** Method for Camera */
+class camera{
+
+
+}
+
+/** Method for Move Motors */
+class robotMotors{
+
+}
+
+ 
